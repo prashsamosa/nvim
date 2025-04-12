92a9860b60f267cb93a6aff3dfd04f7840ceafc4
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-04-12 17:07:49 +0000

    fixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,73 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  }
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      -- Add languages to be installed here: https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+      ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "tsx", "javascript", "typescript", "vimdoc", "vim", "bash", "html", "css", "json", "yaml", "markdown", "markdown_inline" },
+
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+
+      highlight = { enable = true },
+      indent = { enable = true },
+
+      -- Setup incremental selection (for <C-space> and <bs> mappings)
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false, -- Optional: disable scope incremental selection
+          node_decremental = "<bs>",
+        },
+      },
+
+      -- Setup textobjects (like 'af', 'if', 'ac', 'ic')
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ao"] = "@comment.outer",                             -- Added comment textobject
+            ["as"] = { query = "@scope", query_group = "locals" }, -- Added scope textobject
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner", -- Use <leader>a for next parameter
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner", -- Use <leader>A for previous parameter
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+          },
+        },
+      },
+    })
+  end,
+}
