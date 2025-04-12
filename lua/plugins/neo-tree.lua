8c9e4f18649b5f813cabcce2237e15daa6b30775
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-04-12 00:14:29 +0000

    added neo tree shortcuts

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,58 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  opts = {
+    sources = { "filesystem", "buffers", "git_status" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+    bind_to_cwd = false,
+    follow_current_file = { enabled = true },
+    use_libuv_file_watcher = true,
     },
-    lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    ---@type neotree.Config?
-    opts = {
-      -- fill any relevant options here
+    window = {
+    mappings = {
+      ["l"] = "open",
+      ["h"] = "close_node",
+      ["<space>"] = "none",
+      ["Y"] = {
+      function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        vim.fn.setreg("+", path, "c")
+      end,
+      desc = "Copy Path to Clipboard",
+      },
+      ["O"] = {
+      function(state)
+        require("lazy.util").open(state.tree:get_node().path, { system = true })
+      end,
+      desc = "Open with System Application",
+      },
+      ["P"] = { "toggle_preview", config = { use_float = false } },
     },
+    },
+    default_component_configs = {
+    indent = {
+      with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+      expander_collapsed = "",
+      expander_expanded = "",
+      expander_highlight = "NeoTreeExpander",
+    },
+    git_status = {
+      symbols = {
+      unstaged = "󰄱",
+      staged = "󰱒",
+      },
+    },
+    },
+  },
   }
