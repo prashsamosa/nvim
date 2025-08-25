3b6f240053c12f15b9815934361d30c76c14811a
prashsamosa <prashanttbhardwajj@gmail.com>
2025-08-26 04:40:51 +0530

    removed lsp folder

--- lsp/emmet_ls.lua
+++
@@ -1,6 +0,0 @@
-return {
-    cmd = { 'emmet-ls', '--stdio' },
-    root_markers = { '.git' },
-    filetypes = { 'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'svelte', 'vue' },
-    settings = {},
-}
