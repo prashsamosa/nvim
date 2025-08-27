ffee1a6a333f16da67c7789040de76b700c8e69f
prashsamosa <prashanttbhardwajj@gmail.com>
2025-08-27 08:33:46 +0530

    fixed barbar padding

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,3 +1,4 @@
+-- lua/plugins/barbar.lua
 return {
     "romgrk/barbar.nvim",
     dependencies = {
@@ -6,26 +7,33 @@ return {
     },
     event = "BufReadPost",
 
-    keys = {
-        { "<A-,>", "<cmd>BufferPrevious<cr>" },
-        { "<A-.>", "<cmd>BufferNext<cr>" },
-        { "<A-S-,>", "<cmd>BufferMovePrevious<cr>" },
-        { "<A-S-.>", "<cmd>BufferMoveNext<cr>" },
-        { "<A-p>", "<cmd>BufferPin<cr>" },
-        { "<leader>bO", "<cmd>BufferCloseAllButCurrentOrPinned<cr>" },
-    },
-
+    -- IMPORTANT: disable barbar's auto-setup, LazyVim will call setup with opts
     init = function()
+        vim.g.barbar_auto_setup = false
+
+        -- Alt+[1-9] = jump to buffer
         for i = 1, 9 do
             vim.keymap.set("n", ("<A-%d>"):format(i), ("<cmd>BufferGoto %d<cr>"):format(i))
         end
+        -- Alt+0 = last buffer
         vim.keymap.set("n", "<A-0>", "<cmd>BufferLast<cr>")
     end,
 
+    keys = {
+        { "<A-,>",      "<cmd>BufferPrevious<cr>",                   desc = "Prev buffer" },
+        { "<A-.>",      "<cmd>BufferNext<cr>",                       desc = "Next buffer" },
+        { "<A-S-,>",    "<cmd>BufferMovePrevious<cr>",               desc = "Move buffer left" },
+        { "<A-S-.>",    "<cmd>BufferMoveNext<cr>",                   desc = "Move buffer right" },
+        { "<A-p>",      "<cmd>BufferPin<cr>",                        desc = "Pin buffer" },
+        { "<leader>bO", "<cmd>BufferCloseAllButCurrentOrPinned<cr>", desc = "Close all but current/pinned" },
+    },
+
     opts = {
+        animation = true,
         auto_hide = true,
         clickable = true,
         insert_at_end = true,
+        maximum_padding = 1,
         icons = {
             buffer_index = true,
             button = "×",
