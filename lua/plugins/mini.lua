4855911fced5bc86e19857b50750452531b7d563
prashsamosa <prashanttbhardwajj@gmail.com>
2025-08-28 07:39:01 +0530

    fixed mini.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,126 +1,29 @@
 return {
-  -- Text objects and AI
-  {
-    "echasnovski/mini.ai",
-    version = false,
-    event = "VeryLazy",
-    config = true,
-  },
-
-  -- Surround operations
-  {
-    "echasnovski/mini.surround",
-    version = false,
-    event = "VeryLazy",
-    opts = {
-      mappings = {
-        add = "gsa",
-        delete = "gsd",
-        replace = "gsr",
-      },
-    },
-  },
-
-  -- Auto pairs
-  {
-    "echasnovski/mini.pairs",
-    version = false,
-    event = "VeryLazy",
-    config = true,
-  },
-
-  -- Indent scope visualization
-  {
-    "echasnovski/mini.indentscope",
-    version = false,
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
+  "echasnovski/mini.nvim",
+  version = false,
+  config = function()
+    require("mini.indentscope").setup({
       symbol = "╎",
       draw = { animation = require("mini.indentscope").gen_animation.none() },
-    },
-  },
-
-  -- Move lines and selections
-  {
-    "echasnovski/mini.move",
-    version = false,
-    keys = {
-      { "<A-h>", mode = { "n", "v" } },
-      { "<A-j>", mode = { "n", "v" } },
-      { "<A-k>", mode = { "n", "v" } },
-      { "<A-l>", mode = { "n", "v" } },
-    },
-    config = true,
-  },
-
-  -- Split/join operations
-  {
-    "echasnovski/mini.splitjoin",
-    version = false,
-    keys = { "gS" },
-    opts = {
-      mappings = { toggle = "gS" },
-    },
-  },
-
-  -- Alignment operations
-  {
-    "echasnovski/mini.align",
-    version = false,
-    keys = { { "ga", mode = { "n", "v" } } },
-    config = true,
-  },
-
-  -- Various operators
-  {
-    "echasnovski/mini.operators",
-    version = false,
-    keys = {
-      { "g=", desc = "Evaluate" },
-      { "gx", desc = "Exchange" },
-      { "gm", desc = "Multiply" },
-      { "gr", desc = "Replace" },
-      { "gs", desc = "Sort" },
-    },
-    opts = {
+    })
+    require("mini.ai").setup()
+    require("mini.pairs").setup()
+    require("mini.surround").setup({ mappings = { add = "gsa", delete = "gsd", replace = "gsr" } })
+    require("mini.move").setup()
+    require("mini.splitjoin").setup({ mappings = { toggle = "gS" } })
+    require("mini.align").setup()
+    require("mini.operators").setup({
       evaluate = { prefix = "g=" },
       exchange = { prefix = "gx" },
       multiply = { prefix = "gm" },
       replace = { prefix = "gr" },
       sort = { prefix = "gs" },
-    },
-  },
-
-  -- Sessions management
-  {
-    "echasnovski/mini.sessions",
-    version = false,
-    lazy = false,
-    opts = {
+    })
+    require("mini.sessions").setup({
       autoread = false,
       autowrite = true,
       directory = vim.fn.stdpath('data') .. '/sessions/',
       file = 'Session.vim',
-    },
-    keys = {
-      { "<leader>Ss", function() require("mini.sessions").write() end,  desc = "Save session" },
-      { "<leader>Sr", function() require("mini.sessions").read() end,   desc = "Read session" },
-      { "<leader>Sd", function() require("mini.sessions").delete() end, desc = "Delete session" },
-      { "<leader>Sl", function() require("mini.sessions").select() end, desc = "Select session" },
-      {
-        "<leader>Sc",
-        function()
-          require("mini.sessions").write(vim.fn.input("Session name: "))
-        end,
-        desc = "Save named session"
-      },
-      {
-        "<leader>S.",
-        function()
-          require("mini.sessions").write(vim.fn.fnamemodify(vim.fn.getcwd(), ":t"))
-        end,
-        desc = "Save directory session"
-      },
-    },
-  },
+    })
+  end,
 }
