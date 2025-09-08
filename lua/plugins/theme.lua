357f2430a638e8b4be5f4cb664e83eca83274f8a
prashsamosa <prashanttbhardwajj@gmail.com>
2025-09-09 01:22:49 +0530

    new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,11 @@
--- ================================================================================================
--- TITLE : melange-nvim
--- ABOUT : A subtle, warm colorscheme for Neovim inspired by Sublime Text's Melange theme.
--- LINKS :
---   > github : https://github.com/savq/melange-nvim
--- ================================================================================================
-
 return {
-	"savq/melange-nvim",
+	"scottmckendry/cyberdream.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme melange")
+		require("cyberdream").setup({
+			variant = "default",
+		})
+		vim.cmd("colorscheme cyberdream")
 	end,
 }
